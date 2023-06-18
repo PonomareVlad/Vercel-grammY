6430ceba42c87ba515e49b73bbbe8b61ee9b1b29
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-06-19 02:15:38 +0500

    safeStart

--- index.mjs
+++ index.mjs
@@ -2,20 +2,21 @@ export const devEnvs = ["development"];
 export const {VERCEL_ENV, VERCEL_URL} = process.env;
 export const isEdge = typeof EdgeRuntime === "string";
 
-export const getURL = ({path = "api/update", prefix = "", ...other} = {}) => {
-    return new URL(`${prefix}https://${getHost(other)}/${path}`).href;
+export const safeStart = (bot, {events = ["SIGTERM", "SIGINT"], ...options} = {}) => {
+    if (!isEdge) events.forEach(eventName => process.once(eventName, () => bot.stop()));
+    return bot.start(options);
 }
 
-export const getHost = ({headers = {}, header = "x-forwarded-host", fallback = VERCEL_URL} = {}) => {
-    return (isEdge ? headers?.get?.(header) : headers[header]) || fallback;
+export const setWebhook = (bot, {path, headers, prefix, header, fallback, ...other} = {}) => {
+    const url = getURL({path, headers, prefix, header, fallback});
+    return bot.api.setWebhook(url, other);
 }
 
-export const setWebhook = (bot, {path, prefix, header, fallback, catchErrors, allowEnvs = devEnvs, ...other} = {}) => {
+export const setWebhookCallback = (bot, {catchErrors, allowEnvs = devEnvs, ...other} = {}) => {
     return async ({headers}, {json = jsonResponse}) => {
         try {
             if (!allowEnvs.includes(VERCEL_ENV)) return json({ok: false});
-            const url = getURL({path, headers, prefix, header, fallback});
-            const ok = await bot.api.setWebhook(url, other);
+            const ok = await setWebhook(bot, other);
             return json({ok});
         } catch (e) {
             if (!catchErrors) throw e;
@@ -35,4 +36,12 @@ export const jsonResponse = (value, {space, status, replacer, statusText, header
         statusText,
         status
     });
+}
+
+export const getHost = ({headers = {}, header = "x-forwarded-host", fallback = VERCEL_URL} = {}) => {
+    return (isEdge ? headers?.get?.(header) : headers[header]) || fallback;
+}
+
+export const getURL = ({host, path = "api/update", prefix = "", ...other} = {}) => {
+    return new URL(`${prefix}https://${host || getHost(other)}/${path}`).href;
 }
