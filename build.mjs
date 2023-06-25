5c1470d49db1a381201f9275d12a47b8fa5cd9fd
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-06-26 01:25:51 +0500

    Removing unnecessary methods

--- build.mjs
+++
@@ -1,8 +0,0 @@
-export * from "./index.mjs";
-import {writeFileSync} from "fs";
-
-export const saveInfo = async (bot, {path}) => {
-    const info = await bot.api.getMe();
-    writeFileSync(path, JSON.stringify(info));
-    return info;
-}
--- index.mjs
+++ index.mjs
@@ -1,15 +1,13 @@
-import {promisify} from "util";
 import {webhookCallback} from "grammy";
 
-export const devEnvs = ["development"];
 export const {VERCEL_ENV, VERCEL_URL} = process.env;
-export const isEdge = typeof EdgeRuntime === "string";
 
-export const wait = promisify((a, f) => setTimeout(f, a));
+export const getURL = ({host, path = "api/update", prefix = "", ...other} = {}) => {
+    return new URL(`${prefix}https://${host || getHost(other)}/${path}`).href;
+}
 
-export const safeStart = (bot, {events = ["SIGTERM", "SIGINT"], ...options} = {}) => {
-    if (!isEdge) events.forEach(eventName => process.once(eventName, () => bot.stop()));
-    return bot.start(options);
+export const getHost = ({headers = {}, header = "x-forwarded-host", fallback = VERCEL_URL} = {}) => {
+    return (typeof headers?.get === "function" ? headers?.get?.(header) : headers[header]) || fallback;
 }
 
 export const setWebhook = (bot, {host, path, headers, prefix, header, fallback, ...other} = {}) => {
@@ -17,14 +15,14 @@ export const setWebhook = (bot, {host, path, headers, prefix, header, fallback, 
     return bot.api.setWebhook(url, other);
 }
 
-export const setWebhookCallback = (bot, {catchErrors, allowEnvs = devEnvs, ...other} = {}) => {
+export const setWebhookCallback = (bot, {onError = "throw", allowedEnvs = ["development"], ...other} = {}) => {
     return async ({headers}, {json = jsonResponse}) => {
         try {
-            if (!allowEnvs.includes(VERCEL_ENV)) return json({ok: false});
+            if (!allowedEnvs.includes(VERCEL_ENV)) return json({ok: false});
             const ok = await setWebhook(bot, {headers, ...other});
             return json({ok});
         } catch (e) {
-            if (!catchErrors) throw e;
+            if (onError === "throw") throw e;
             console.error(e);
             return json(e);
         }
@@ -57,12 +55,4 @@ export const jsonResponse = (value, {space, status, replacer, statusText, header
         statusText,
         status
     });
-}
-
-export const getHost = ({headers = {}, header = "x-forwarded-host", fallback = VERCEL_URL} = {}) => {
-    return (isEdge ? headers?.get?.(header) : headers[header]) || fallback;
-}
-
-export const getURL = ({host, path = "api/update", prefix = "", ...other} = {}) => {
-    return new URL(`${prefix}https://${host || getHost(other)}/${path}`).href;
 }
